b2aad61f89e739f1e2a1ad8d9ed1a743508c0540
Milena Franco <milena.franco@enext.com.br>
2019-09-26 15:03:37 -0300

    Inicio do desenvolvimento do menu mobile e ajustes na responsividade

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Merlins-Potions
+﻿# Merlins-Potions
 Página de poções criada com o intuito de pôr em prática todo o conhecimento ganho durante o projeto de capacitação feito na enext.
+
+Foram usados no projeto:
+
+CSS, HTML e Javascript puro, assim como o URL do arquivo json fornecido para extrair informações.
--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// menu dropdown mobile
+
+function showMenuFunction() {
+    document.getElementById('dropdown-itens').classList.toggle('showMenu');
+}
+
+// fechar menu
+window.onclick = function (event) {
+    if (!event.target.matches('.mobile-menu-icon')) {
+        var dropdown = document.getElementsByClassName("mobile-menu-dropdown-itens");
+        for (var i = 0; i < dropdown.length; i++) {
+            var openDropdown = dropdown[i];
+            if (openDropdown.classList.contains('showMenu')) {
+                openDropdown.classList.remove('showMenu');
+            }
+        }
+    }
+}
+
+
 // modal
 
 var modal = document.getElementById('potion-modal');
@@ -44,12 +64,13 @@ function showInfos(id) {
     document.getElementById("potion-price").innerHTML = '$' + myObj.potions[id].price;
     document.getElementById("potion-image").src = 'Products/' + myObj.potions[id].image;
 
+    var ul = document.getElementById('potion-ingredients');
+    ul.innerHTML = '';
     for (let j = 0; j < myObj.potions[id].ingredients.length; j++) {
-        var ul = document.getElementById('potion-ingredients');
         var li = document.createElement('li');
         ul.appendChild(li);
         li.innerHTML = myObj.potions[id].ingredients[j];
     }
-    //ta concatenando em todo click
 }
+
 
